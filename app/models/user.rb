620135b4961b1bd064653b314add0842f1bf21c4
yashvasundhariya <yashvasundhariya.1@gmail.com>
2024-09-25 20:59:04 +0530

    Add custom authentication completely

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+	has_secure_password
+
+	validates :email, presence: true
+
+	normalizes :email, with: ->(email) {email.strip.downcase}
+
+	generates_token_for :password_reset, expires_in: 5.minutes do
+		password_salt&.last(10)
+	end
+
+	generates_token_for :email_confirmation, expires_in: 5.minutes do
+		email
+	end
+end
