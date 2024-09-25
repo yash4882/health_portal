e8a02dfd427e846750b16f7b2a4c5eaf42922b36
yashvasundhariya <yashvasundhariya.1@gmail.com>
2024-09-25 21:06:39 +0530

    adding validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
 
-	validates :email, presence: true
+	validates :email, presence: true, uniqueness: true
 
 	normalizes :email, with: ->(email) {email.strip.downcase}
 
