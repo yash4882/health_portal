7f5be9355c87da9c96ae645d8358eca815376a6b
yashvasundhariya <yashvasundhariya.1@gmail.com>
2024-09-28 17:44:20 +0530

    change some validations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,4 @@
 class Patient < ApplicationRecord
-	validates :name, :age, :gender, :contact, :health_issue, presence: true
+	validates :name, :age, :gender, :health_issue, :blood_type, :height, :weight,  presence: true
+	validates :contact, presence: true, format: { with: /\A\d{10}\z/, message: "must be a 10-digit number" }
 end
